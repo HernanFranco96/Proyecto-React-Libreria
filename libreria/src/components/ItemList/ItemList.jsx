683fb7674183001af33e3979f481b3162da57cdf
HernanFranco96 <hernan.franco051096@gmail.com>
2022-07-09 02:08:51 -0300

    Desafio Catalogo con MAPS y Promises

---
+++ libreria/src/components/ItemList/ItemList.jsx
@@ -0,0 +1,24 @@
+import Item from '../Item/Item';
+import { useState, useEffect } from 'react';
+
+const ItemList = () => {
+    const [libros, setLibros] = useState([]);
+
+    useEffect(() => {
+        fetch('./src/components/ItemList/libros.json')
+            .then((response) => response.json())
+            .then((json) => setLibros(json))
+    }, []);
+
+    return <>
+        <div className="container-fluid">
+            {libros.map((libro) => (
+                <div key={libro.legajo}>
+                    <Item data={libro}/>
+                </div>
+            ))}
+        </div>
+    </>;
+};
+
+export default ItemList;
--- libreria/src/views/ItemListContainer.jsx
+++ libreria/src/views/ItemListContainer.jsx
@@ -1,10 +1,13 @@
 import ItemCount from "../components/ItemCount/ItemCount";
+import ItemList from "../components/ItemList/ItemList";
 
 const ItemListContainer = (props) => {
+
     return <>
         <div className="alert alert-primary" role="alert">
             {props.saludo}
             <ItemCount stock={5} inicio={1}/>
+            <ItemList />
         </div>
     </>;
 };
