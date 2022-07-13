23a325071417f33d36388566f533b190badb4534
Hernán Franco <hernan.franco051096@gmail.com>
2022-07-13 20:33:11 -0300

    Add files via upload

--- libreria/src/components/ItemList/ItemList.jsx
+++ libreria/src/components/ItemList/ItemList.jsx
@@ -1,23 +1,8 @@
 import Item from '../Item/Item';
-import { useState, useEffect } from 'react';
 
-const ItemList = () => {
-    const [libros, setLibros] = useState([]);
-
-    useEffect(() => {
-        fetch('./src/libros.json')
-            .then((response) => response.json())
-            .then((json) => setLibros(json))
-    }, []);
-
+const ItemList = ({libro}) => {
     return <>
-        <div className="container-fluid">
-            {libros.map((libro) => (
-                <div key={libro.legajo}>
-                    <Item data={libro}/>
-                </div>
-            ))}
-        </div>
+        <Item data={libro}/>
     </>;
 };
 
--- libreria/src/views/ItemListContainer.jsx
+++ libreria/src/views/ItemListContainer.jsx
@@ -1,16 +1,31 @@
 import ItemCount from "../components/ItemCount/ItemCount";
 import ItemList from "../components/ItemList/ItemList";
+import { useState, useEffect } from 'react';
 
 const ItemListContainer = (props) => {
     const onAdd = (count) => {
         (count > 0 && count <= 5) ? alert('Usted selecciono un total de: ' + count) : null;
     };
 
+    const [libros, setLibros] = useState([]);
+
+    useEffect(() => {
+        fetch('./src/libros.json')
+            .then((response) => response.json())
+            .then((json) => setLibros(json))
+    }, []);
+
     return <>
         <div className="alert alert-primary" role="alert">
             {props.saludo}
             <ItemCount stock={5} initial={1} onAdd={onAdd}/>
-            <ItemList />
+            <div className="container-fluid">
+                {libros.map((lib) => (
+                    <div key={lib.legajo}>
+                        <ItemList libro={lib}/>
+                    </div>
+                ))}
+            </div>
         </div>
     </>;
 };
