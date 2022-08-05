61645b0e18e624c1a8bbb12b204d285d337caabc
HernanFranco96 <hernan.franco051096@gmail.com>
2022-08-05 00:13:02 -0300

    Quitamos alert

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -38,20 +38,14 @@ const CartContextProvider = ( {children} ) => {
         setCartList([])
     };
 
-    const message = (libro, valor) => {
-        valor === 1 ? alert(`Se agregaron ${libro.cantidad} libros de "${libro.titulo}" al carrito.`) : alert(`Se acumularon ${libro.cantidad} libros mas de "${libro.titulo}" al carrito.`)
-    }
-
     const isInCart = (id) => {
         let libro = cartList.find(lib => lib.legajo === id.legajo)
         if(libro !== undefined){
             let index = cartList.indexOf(libro);
             cartList[index].cantidad += id.cantidad;
-            message(id, 0);
             return -1;
         }
 
-        message(id, 1);
         setCartList([
             ...cartList,
             id
@@ -60,7 +54,6 @@ const CartContextProvider = ( {children} ) => {
 
     const addToCart = (objProducto) =>{
         if(cartList.length === 0){
-            message(objProducto, 1);
             setCartList([
                 ...cartList,
                 objProducto
